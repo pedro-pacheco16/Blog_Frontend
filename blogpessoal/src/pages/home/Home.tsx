0bedb9aa5087de17b5ad8038b192ea086576d833
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-10-25 23:57:30 -0300

    adicionando o Tailwind

--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -3,16 +3,15 @@ import'./Home.css';
   function Home() {
     return (
       <>
-          <div className='main'>
+          <div className="w-screen flex justify-center">
               <div>
-                  <div className='secundaria'>
+                  <div className="max-w-7xl flex flex-col items-center">
                       <h2 className='titulo'>Seja Bem Vindo!</h2>
                       <p>Compartilhe aqui suas experiências e curiosidades!</p>
                   </div>
 
-                  <div className='secundaria'>
+                  <div className="max-w-7xl flex flex-col items-center">
                       <img 
-                          className='img'
                           src=".\public\innovation-amico.png"
                           alt="Imagem de uma lampada do conhecimento" 
                           width="400px"
