1664a7dd7a3d289cef7d3f50f86d890b1fd84258
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-10-26 19:21:10 -0300

    Adicionando login cadastro e rotas

--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -1,22 +1,26 @@
+import { useContext } from "react"
+import { AuthContext } from "../../context/AuthContext"
+
 function Home() {
+    
+    const { nome } = useContext(AuthContext)
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
+                            Seja Bem Vinde, {nome}!
                         </h2>
                         <p className='text-xl'>
                             Expresse aqui seus pensamentos e opniões
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
+                        <button className='rounded bg-white text-blue-800 py-2 px-4'
                                 >
-                                Nova Postagem
-                            </div>
+                                Ver Postagens
+                            </button>
                         </div>
                     </div>
 
